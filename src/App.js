c499b7fa461ba22cf85999556f6aa1a20b05e635
OmTejaswi <75362653+OmTejaswi@users.noreply.github.com>
2021-09-05 16:49:30 +0530

    categories

--- src/App.js
+++ src/App.js
@@ -2,13 +2,52 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  HashRouter
+} from "react-router-dom";
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <News pageSize={20} category='science' country='in'/>
+        <Router>
+          <Navbar />
+          
+          <Switch>
+            {/* India */}
+            <Route exact path='/'>
+              <News key='general' pageSize={20} category='general' country='in' />
+            </Route>
+            <Route exact path='/business'>
+              <News key='business' pageSize={20} category='business' country='in' />
+            </Route>
+            <Route exact path='/entertainment'>
+              <News key='entertainment' pageSize={20} category='entertainment' country='in' />
+            </Route>
+            <Route exact path='/general'>
+              <News key='general' pageSize={20} category='general' country='in' />
+            </Route>
+            <Route exact path='/health'>
+              <News key='health' pageSize={20} category='health' country='in' />
+            </Route>
+            <Route exact path='/science'>
+              <News key='science' pageSize={20} category='science' country='in' />
+            </Route>
+            <Route exact path='/sports'>
+              <News key='sports' pageSize={20} category='sports' country='in' />
+            </Route>
+            <Route exact path='/technology'>
+              <News key='technology' pageSize={20} category='technology' country='in' />
+            </Route>
+            {/* <Route exact path='/international'>
+              <News key='international' pageSize={20} category='international' country='in' />
+            </Route> */}
+          </Switch>
+        </Router>
       </div>
     )
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,60 +1,53 @@
 import React, { Component } from 'react'
+import {
+    Link,
+} from "react-router-dom";
 
 export class Navbar extends Component {
     render() {
+
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#">NewsMonkey</a>
+                        <Link className="navbar-brand" to="/">NewsMonkey</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
-                                <li className="nav-item dropdown">
-                                    <a className="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Countries
-                                    </a>
-                                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <li><a className="dropdown-item" href="#">India</a></li>
-                                        <li><a className="dropdown-item" href="#">United States</a></li>
-                                        <li><a className="dropdown-item" href="#">United Kingdom</a></li>
-                                        <li><a className="dropdown-item" href="#">Russia</a></li>
-                                        <li><a className="dropdown-item" href="#">France</a></li>
-                                        <li><a className="dropdown-item" href="#">Australia</a></li>
-                                        <li><hr className="dropdown-divider" /></li>
-                                        <li><a className="dropdown-item" href="#">International</a></li>
-                                    </ul>
-                                </li>
+                                {/* <select id="mySelect">
+                                    <option value="in">India</option>
+                                    <option value="us">United States</option>
+                                    <option value="gb">United Kingdom</option>
+                                    <option value="ru">Russia</option>
+                                    <option value="fr">France</option>
+                                    <option value="au">Australia</option>
+                                    <option value="International">International</option>
+                                </select> */}
 
-
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">About</a>
-                                </li>
-
-                                <li className="nav-item"><a className="nav-link" href="#">business </a></li>
-                                <li className="nav-item"><a className="nav-link" href="#">entertainment </a></li>
-                                <li className="nav-item"><a className="nav-link" href="#">general </a></li>
-                                <li className="nav-item"><a className="nav-link" href="#">health </a></li>
-                                <li className="nav-item"><a className="nav-link" href="#">science </a></li>
-                                <li className="nav-item"><a className="nav-link" href="#">sports </a></li>
-                                <li className="nav-item"><a className="nav-link" href="#">technology </a></li>
+                                <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/health"></Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
                             </ul>
                             <form className="d-flex">
                                 {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                                 <button className="btn btn-outline-success" type="submit">Search</button> */}
 
                                 <li className="nav-item nav-link dropdown mx-2">
-                                    <a className="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" style={{ color: '#f1f1f1' }} data-bs-toggle="dropdown" aria-expanded="false">
+                                    <Link className="nav-link dropdown-toggle active" to="#" id="navbarDropdown" role="button" style={{ color: '#f1f1f1' }} data-bs-toggle="dropdown" aria-expanded="false">
                                         Page Size
-                                    </a>
+                                    </Link>
                                     <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <li><a className="dropdown-item">5</a></li>
-                                        <li><a className="dropdown-item">10</a></li>
-                                        <li><a className="dropdown-item">15</a></li>
-                                        <li><a className="dropdown-item">20</a></li>
+                                        <li><Link className="dropdown-item">5</Link></li>
+                                        <li><Link className="dropdown-item">10</Link></li>
+                                        <li><Link className="dropdown-item">15</Link></li>
+                                        <li><Link className="dropdown-item">20</Link></li>
                                     </ul>
                                 </li>
                             </form>
