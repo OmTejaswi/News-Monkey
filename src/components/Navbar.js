c39941a7640922a25da233a907dbcf139cb4c9c0
OmTejaswi <75362653+OmTejaswi@users.noreply.github.com>
2021-09-10 10:53:43 +0530

    navbar bug fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export class Navbar extends Component {
     render() {
         return (
             <div style={{marginBottom: '80px'}}>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" style={{maxHeight: '60px'}}>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                     <div className="container-fluid">
                         <Link className="navbar-brand notranslate" to="/">NewsMonkey</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -36,7 +36,7 @@ export class Navbar extends Component {
                                 <li className="nav-item"><Link className="nav-link" to="/feedback">Feedback</Link></li>
                             </ul>
                             <form className="d-flex">
-                                <li className="nav-item nav-link" style={{ color: 'white' }}><div id="google_translate_element"></div></li>
+                                <li className="nav-item nav-link"><div id="google_translate_element" style={{ color: 'white', height: '30px', position: 'relative', bottom: '0px' }}></div></li>
                                 {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" id='search'/>
                                 <button className="btn btn-outline-success" type="submit">Search</button> */}
 
