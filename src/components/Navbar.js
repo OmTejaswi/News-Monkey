61c6dba3ba002b2c0efbc43fb6c33a7fc17c350c
OmTejaswi <75362653+OmTejaswi@users.noreply.github.com>
2021-09-11 16:43:00 +0530

    Look improved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,8 +30,8 @@ export class Navbar extends Component {
                                 <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
                                 <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
                                 <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-                                <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
                                 <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
                                 <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
                                 <li className="nav-item"><Link className="nav-link" to="/feedback">Feedback</Link></li>
                             </ul>
